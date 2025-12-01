744c71b35d7d4a74301b7334a7145798d1f0c15f
NebulousPsychology <78229921+NebulousPsychology@users.noreply.github.com>
2025-12-01 13:19:13 -0800

    working on pwm, inconclusive

--- src/lib.rs
+++ src/lib.rs
@@ -106,16 +106,38 @@ pub mod pico_shield {
             rp_pico::hal::gpio::PullDown,
         >,
     >;
-    // chA (even pin, pwmout only),  chB (odd pin, pwmio)
-    // impl  sbc_motor_shield::MotorShieldConfigurationBuilder<> {
 
-    // }
     /// Initialize the sb-components motor shield.
     /// Mapped according to the geekpi pico-to-rpi-40pin-hat breakout board
     pub fn create(
         pins: rp_pico::Pins,
         pwm_slices: rp_pico::hal::pwm::Slices,
     ) -> Result<PicoGeeekpiSbcShield, sbc_motor_shield::MotorShieldError> {
+        // do pwm configurations <see rp2040-hal::pwm>
+        let mut pwm_m1_3b = pwm_slices.pwm3.channel_b;
+        pwm_m1_3b.output_to(pins.gpio7);
+        let mut pwm_m2_3a = pwm_slices.pwm3.channel_a;
+        pwm_m2_3a.output_to(pins.gpio22);
+        let mut pwm_m3_1b = pwm_slices.pwm1.channel_b;
+        pwm_m3_1b.output_to(pins.gpio3);
+        let mut pwm_m4_1a = pwm_slices.pwm1.channel_a;
+        pwm_m4_1a.output_to(pins.gpio18);
+        /*
+        from  <see rp2040-hal::pwm>
+        [
+        Pwm0: (0, [Gpio0, Gpio1, Gpio16, Gpio17], 0),
+        Pwm1: (1, [Gpio2, Gpio3, Gpio18, Gpio19], 1),
+        Pwm2: (2, [Gpio4, Gpio5, Gpio20, Gpio21], 2),
+        Pwm3: (3, [Gpio6, Gpio7, Gpio22, Gpio23], 3),
+        Pwm4: (4, [Gpio8, Gpio9, Gpio24, Gpio25], 4),
+        Pwm5: (5, [Gpio10, Gpio11, Gpio26, Gpio27], 5),
+        Pwm6: (6, [Gpio12, Gpio13, Gpio28, Gpio29], 6),
+        Pwm7: (7, [Gpio14, Gpio15], 7)
+        ]
+        chA (even pin, pwmout only),  chB (odd pin, pwmio)
+         */
+
+        // Produce the motor shield
         let pico_shield = sbc_motor_shield::MotorShieldConfigurationBuilder::new()
             .with_ir1(pins.gpio6.into_pull_up_input())
             .with_ir2(pins.gpio28.into_pull_up_input())
@@ -128,8 +150,8 @@ pub mod pico_shield {
             //     pins.gpio16.into_floating_input(), // pull type not specified?
             // )
             .with_lights(
-                pins.gpio14.into_push_pull_output(), // f
-                pins.gpio12.into_push_pull_output(), // b
+                pins.gpio14.into_push_pull_output(), // F
+                pins.gpio12.into_push_pull_output(), // B
                 pins.gpio13.into_push_pull_output(), // L
                 pins.gpio17.into_push_pull_output(), // R
             )
@@ -137,27 +159,26 @@ pub mod pico_shield {
                 // 5(gp3) is available?
                 pins.gpio9.into_push_pull_output(),
                 pins.gpio8.into_push_pull_output(),
-                // pwm_slices.pwm3.channel_b.output_to(pins.gpio7), // TODO: find gp7 among slices [6,7,22,23]:pwm3
-                pwm_slices.pwm3.channel_b, // TODO: find gp7 among slices [6,7,22,23]:pwm3
+                pwm_m1_3b, // TODO: find gp7 among slices [6,7,22,23]:pwm3
                 Some(1000),
             )
             .with_motor2(
                 pins.gpio27.into_push_pull_output(),
                 pins.gpio26.into_push_pull_output(),
-                pwm_slices.pwm3.channel_a, // TODO: find gp 22 among slices ! also pwm3?
+                pwm_m2_3a, // TODO: find gp 22 among slices ! also pwm3?
                 Some(1000),
             )
             // chA (even pin, pwmout only),  chB (odd pin, pwmio)
             .with_motor3(
                 pins.gpio4.into_push_pull_output(),
                 pins.gpio2.into_push_pull_output(),
-                pwm_slices.pwm1.channel_b, // TODO: find gp 3 among slices {2,3,18,19}: pwm1
+                pwm_m3_1b, // TODO: find gp 3 among slices {2,3,18,19}: pwm1
                 Some(1000),
             )
             .with_motor4(
                 pins.gpio5.into_push_pull_output(),
                 pins.gpio19.into_push_pull_output(),
-                pwm_slices.pwm1.channel_a, // TODO: find gp 18 among slices !also pwm1 {2,3,18,19}: pwm1
+                pwm_m4_1a, // TODO: find gp 18 among slices !also pwm1 {2,3,18,19}: pwm1
                 Some(1000),
             )
             // .with_i2c(sda:pins.gpio20, scl:pins.gpio21.i2c) // I2C #0
